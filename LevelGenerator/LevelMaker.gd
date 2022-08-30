b64b39a35d38661cded6394f950f8b1a6c05d75d
Tylere Couture <tylerecouture@gmail.com>
2022-08-30 15:47:06 -0700

    basic working level generator

--- LevelGenerator/LevelMaker.gd
+++ LevelGenerator/LevelMaker.gd
@@ -1,18 +1,24 @@
 tool
-extends Node2D
+extends TileMap
+
 
 export var WallScene: PackedScene
 export var map_width := 32 setget set_width
 export var map_height := 18 setget set_height
+export var density := 0.2
+
+export var regenerate := false setget set_regenerate
+
+var rng := RandomNumberGenerator.new()
 
 var tile_size : Vector2
 
 var wall : Node2D
 
 func _ready():
-	wall = WallScene.instance()
-	tile_size = Vector2(wall.texture.get_width(), wall.texture.get_height())
-	print("Tile Size: ", tile_size)
+#	wall = WallScene.instance()
+#	tile_size = Vector2(wall.texture.get_width(), wall.texture.get_height())
+#	print("Tile Size: ", tile_size)
 	generate_map()
 
 func set_width(var new_val):
@@ -22,10 +28,25 @@ func set_width(var new_val):
 func set_height(var new_val):
 	map_height = new_val
 	generate_map()
+	
+		
+func set_regenerate(var new_val):
+	generate_map()
+
 
 func generate_map():
+	# _ready() doesn't work because it's called when the "tool" is opened, not necessarily everytime the tool is played
+	self.clear()
+	rng.randomize()
 	print("Bleep bloop generating map...")
-
+	
+	for x in range(map_width):
+		for y in range(map_height):
+			if randf() > density:			
+				self.set_cell(x, y, 0)
+#			else:
+#				set_cell(x, y, -1)()
+	self.update_bitmask_region()
 #	for node in get_children():
 #		node.queue_free()
 #
@@ -37,4 +58,3 @@ func generate_map():
 #			print(wall_position)
 #			wall.set_position(wall_position)
 #
-
