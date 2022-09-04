dd0f5afcf1e3777d95eec25a099c178b114594fa
kamrancouture <65361792+kamrancouture@users.noreply.github.com>
2022-09-03 21:27:39 -0700

    Summoner spawn bomber

--- Game.gd
+++ Game.gd
@@ -1,8 +1,1 @@
 extends Node2D
-
-onready var player = $Player
-onready var nav = $Navigation2D
-
-
-func _on_Enemy_Path_Timer_timeout():
-	get_tree().call_group("Enemy", "get_target_path", player.global_position)
