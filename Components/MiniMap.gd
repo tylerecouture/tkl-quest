aa872c2a12e1624bda6f33a402a829af70137d0b
Tylere Couture <tylerecouture@gmail.com>
2022-09-04 22:57:15 -0700

    minimap Fog of Wor working

--- Components/MiniMap.gd
+++ Components/MiniMap.gd
@@ -2,7 +2,10 @@ extends MarginContainer
 
 export (NodePath) var player_path
 export (NodePath) var tilemap_path
+export (Color) var fog_color
+export (Color) var map_color
 #export var zoom := 1.5
+export (int) var sight_distance = 10
 
 onready var player: KinematicBody2D = get_node(player_path)
 onready var tilemap: TileMap = get_node(tilemap_path)
@@ -10,31 +13,34 @@ onready var tilemap: TileMap = get_node(tilemap_path)
 var tilemap_bounds: Rect2
 var tilemap_size: Vector2
 
-onready var rect := $Frame/MarginContainer/TextureRect
 onready var player_marker := $Frame/MarginContainer/TextureRect/PlayerMarker
+
+onready var rect := $Frame/MarginContainer/TextureRect
+onready var texture = rect.texture
+
 #onready var mob_marker := $MarginContainer/TextureRect/MobMarker
 #onready var alert_marker := $MarginContainer/TextureRect/AlertMarker
 
-onready var texture = rect.texture
+onready var fog_texture_rect = $Frame/MarginContainer/FogTextureRect
+onready var fog_texture = fog_texture_rect.texture
 
 #onready var icons := {"mob": mob_marker, "alert": alert_marker}
 
 var markers := {}
+var map_fog = []
 var map_texture_size : Vector2 # the original texture size so we don't "print" beyond it
 var map_center : Vector2
 var map_offset : Vector2
 var map_scale
+var map_texture_rect : Rect2
 
 func _ready():
 	map_texture_size = rect.rect_size
-	map_center = map_texture_size / 2	
+	map_texture_rect = Rect2(Vector2(0,0), map_texture_size)
 	
-	# initialize an image for this texture so we can update later
-	# https://docs.godotengine.org/en/stable/classes/class_imagetexture.html#class-imagetexture-method-set-data
-	var image = Image.new()
-	image.create(map_texture_size.x, map_texture_size.y, true, Image.FORMAT_RGBA8)
-	image.fill(Color.red)
-	texture.create_from_image(image) 
+	# initialize an images forthe textures so we can update later
+	texture.create_from_image(create_blank_minimap_image()) 
+	fog_texture.create_from_image(create_blank_minimap_image())
 	
 	# find the bounds of the tilemap
 	if tilemap:
@@ -46,38 +52,36 @@ func _ready():
 	tilemap_size = tilemap_bounds.size
 	
 	map_scale = calc_map_scale()
+	initialize_fog()
 	
 func _process(delta):
 	if player:
 		player_marker.position = get_player_minimap_position()
 		player_marker.rotation = player.rotation
-		# convert the player's position into a tilemap coord
-		# https://docs.godotengine.org/en/stable/classes/class_tilemap.html#class-tilemap-method-world-to-map
-		
-		map_offset = get_player_tilemap_coords() - map_center
+		clear_fog()
 	
 	if tilemap:
 		
 		# https://docs.godotengine.org/en/stable/classes/class_image.html#class-image-method-set-pixelv
-		var image := Image.new()
-
-		# https://github.com/godotengine/godot/issues/15374#issuecomment-355681002
-#		image.create(
-#			tilemap_bounds.end.x - tilemap_bounds.position.x + 1, 
-#			tilemap_bounds.end.y - tilemap_bounds.position.y + 1, 
-#			true, Image.FORMAT_RGBA8
-#		)
-		image.create(map_texture_size.x, map_texture_size.y, true, Image.FORMAT_RGBA8)
-
+		var image := create_blank_minimap_image()
 		for cell in tilemap.get_used_cells():
 			image.lock()
-			image.set_pixelv(cell - tilemap_bounds.position, Color.blue)
+			image.set_pixelv(cell - tilemap_bounds.position, map_color)
 			image.unlock()
-			
-		# Resize the map image to match the texture
 		texture.set_data(image)
 		
+		# redraw the fog texture's image
+		image = create_blank_minimap_image()
+		for x in range(map_fog.size()):
+			for y in range(map_fog[0].size()):
+				if map_fog[x][y]:
+					image.lock()
+					image.set_pixel(x, y, fog_color) # clear the fog
+					image.unlock()
+		fog_texture.set_data(image)
+		
 		rect.rect_scale = Vector2(map_scale, map_scale)
+		fog_texture_rect.rect_scale = Vector2(map_scale, map_scale)
 		
 func get_player_tilemap_coords():
 	var local_pos = tilemap.to_local(player.global_position)
@@ -90,3 +94,37 @@ func calc_map_scale():
 	var scale_x = map_texture_size.x / tilemap_size.x
 	var scale_y = map_texture_size.y / tilemap_size.y
 	return min(scale_x, scale_y)
+	
+func create_blank_minimap_image(fill_color:=Color(0,0,0,0)) -> Image:
+	# https://docs.godotengine.org/en/stable/classes/class_imagetexture.html#class-imagetexture-method-set-data
+	var image = Image.new()
+	image.create(map_texture_size.x, map_texture_size.y, true, Image.FORMAT_RGBA8)
+	image.fill(fill_color)
+	return image
+	
+func initialize_fog():
+	# sets every location to true = not visited = foggy
+	for x in range(map_texture_size.x):
+		map_fog.append([])
+		for y in range(map_texture_size.y):
+			map_fog[x].append([true])
+			
+func clear_fog():
+	# start by scanning a square of pixels around the player
+#	print(player_marker.position)
+	map_fog[player_marker.position.x][player_marker.position.y] = false
+	for x in range(-sight_distance, sight_distance):
+		for y in range(-sight_distance, sight_distance):
+			var p = Vector2(x,y) + player_marker.position
+#			if p.x >=0 and p.y >=0 and p.x < map_texture_size.x and p.y < map_texture_size.y:
+			if map_texture_rect.has_point(p):
+				if p.distance_to(player_marker.position) < sight_distance:
+					map_fog[p.x][p.y] = false
+			
+#			var p := Vector2(x, y)
+			# but only count if within radius
+#			if p.length() < sight_distance:
+#				var point = p + player_marker.position
+#				if map_texture_rect.has_point(point):  # Make sure we don't go outside of the image
+#					map_fog[x][y] = false
+#					print(x,y)
